ae941f1bbad68f32fa76635adfbc5b8dd065d744
Jaqueline S.P.C <jakchellyrick@gmail.com>
2023-05-19 01:11:14 -0300

    Loop
    O loop for percorre cada candidato na lista de candidatos.

--- Projetoindividual-M2.py
+++ Projetoindividual-M2.py
@@ -1,2 +1,8 @@
 def buscar_candidatos(notamin_e=5, notamin_t=5, notamin_p=5, notamin_s=5):
     candidatos_selecionados = []
+    
+    for candidato in candidatos:
+        nome = candidato["nome"]
+        notas = candidato["notas"].split("_")[1:]
+        notas_int = [int(nota[1:]) for nota in notas]
+        
