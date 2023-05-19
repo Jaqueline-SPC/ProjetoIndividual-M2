749337f4f25a0dfb2e8ce87869def416c18a5740
Jaqueline S.P.C <jakchellyrick@gmail.com>
2023-05-19 01:30:10 -0300

    Lista de candidatos
    Contem os nomes dos candidatos selecionados e suas notas
    
    O código depois da função solicita as notas mínimas que o usuário deseja

--- Projetoindividual-M2.py
+++ Projetoindividual-M2.py
@@ -9,3 +9,13 @@ def buscar_candidatos(notamin_e=5, notamin_t=5, notamin_p=5, notamin_s=5):
         if all(nota >= notamin for nota, notamin in zip(notas_int, [notamin_e, notamin_t, notamin_p, notamin_s])):
             candidatos_selecionados.append(nome)
             return candidatos_selecionados
+        
+candidatos = [{"nome": "João", "notas": "e4_t5_p7_s6"},
+             {"nome": "Maria", "notas": "e5_t6_p8_s9"},
+             {"nome": "Pedro", "notas": "e3_t4_p6_s7"},
+             {"nome": "Ana", "notas": "e6_t7_p9_s8"},
+             {"nome": "Carlos", "notas": "e4_t4_p8_s8"},
+             {"nome": "Mariana", "notas": "e7_t8_p9_s9"},]
+            
+        
+        
