5194e6e058cbee2bf6100ad6ec355452548b20a5
Jaqueline S.P.C <jakchellyrick@gmail.com>
2023-05-19 01:18:58 -0300

    If all e zip
    if all verifica se todas as notas atendem aos critérios mínimos fornecidos. zip() é usado para combinar cada nota com a nota mínima correspondente e all() verifica se todas as comparações são verdadeiras.
    
    Se o candidato atender aos critérios, seu nome é adicionado à lista candidatos_selecionados.

--- Projetoindividual-M2.py
+++ Projetoindividual-M2.py
@@ -6,3 +6,6 @@ def buscar_candidatos(notamin_e=5, notamin_t=5, notamin_p=5, notamin_s=5):
         notas = candidato["notas"].split("_")[1:]
         notas_int = [int(nota[1:]) for nota in notas]
         
+        if all(nota >= notamin for nota, notamin in zip(notas_int, [notamin_e, notamin_t, notamin_p, notamin_s])):
+            candidatos_selecionados.append(nome)
+            return candidatos_selecionados
